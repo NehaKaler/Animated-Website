ad3f73a618618d5db2d0ce9ca68152885e3a5eb1
Neha Kaler <nehakaler@gmail.com>
2024-03-30 03:11:25 +0530

    adding commit

--- script.js
+++ script.js
@@ -43,3 +43,74 @@ function loadingAnimation(){
     })
 }
 loadingAnimation()
+
+document.addEventListener("mousemove",function(dets){
+    gsap.to("#cursor",{
+        left:dets.x,
+        top:dets.y
+    })
+})
+
+document.querySelectorAll(".child").forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        gsap.to("#cursor",{
+            transform: "translate(-50%,-50%) scale(1)",
+        });
+    });
+    elem.addEventListener("mouseleave",function(){
+      gsap.to("#cursor",{
+        transform: "translate(-50%,-50%) scale(0)",
+      });
+    });
+});
+// document.querySelector("#child1").addEventListener
+// ("mouseenter",function(){
+//     gsap.to("#cursor",{
+//         transform: `translate(-50%,-50%) scale(1)`
+//     })
+// })
+// document.querySelector("#child1").addEventListener
+// ("mouseleave",function(){
+//     gsap.to("#cursor",{
+//         transform: `translate(-50%,-50%) scale(0)`
+//     })
+// })
+
+// document.querySelector("#child2").addEventListener
+// ("mouseenter",function(){
+//     gsap.to("#cursor",{
+//         transform: `translate(-50%,-50%) scale(1)`
+//     })
+// })
+// document.querySelector("#child2").addEventListener
+// ("mouseleave",function(){
+//     gsap.to("#cursor",{
+//         transform: `translate(-50%,-50%) scale(0)`
+//     })
+// })
+
+// document.querySelector("#child3").addEventListener
+// ("mouseenter",function(){
+//     gsap.to("#cursor",{
+//         transform: `translate(-50%,-50%) scale(1)`
+//     })
+// })
+// document.querySelector("#child3").addEventListener
+// ("mouseleave",function(){
+//     gsap.to("#cursor",{
+//         transform: `translate(-50%,-50%) scale(0)`
+//     })
+// })
+
+// document.querySelector("#child4").addEventListener
+// ("mouseenter",function(){
+//     gsap.to("#cursor",{
+//         transform: `translate(-50%,-50%) scale(1)`
+//     })
+// })
+// document.querySelector("#child4").addEventListener
+// ("mouseleave",function(){
+//     gsap.to("#cursor",{
+//         transform: `translate(-50%,-50%) scale(0)`
+//     })
+// })
